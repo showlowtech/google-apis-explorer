d8fa29b3a24d4a53d0f33d6fd67555084879e6c4
jasonhall@google.com <jasonhall@google.com@dbfd1b55-bec4-91be-7511-bfb63fe70498>
2011-09-08 13:31:30 +0000

    Use GWT 2.4's Callback instead of our own
    Our copy remains so as not to break code that uses it immediately, but it will be removed very soon.
    
    R=moshenko
    DELTA=28  (7 added, 15 deleted, 6 changed)
    
    
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=3200

--- trunk/src/com/google/api/explorer/client/AuthManager.java
+++ trunk/src/com/google/api/explorer/client/AuthManager.java
@@ -22,8 +22,8 @@ import com.google.api.explorer.client.event.AuthGrantedEvent;
 import com.google.api.explorer.client.event.AuthRequestedEvent;
 import com.google.api.gwt.oauth2.client.Auth;
 import com.google.api.gwt.oauth2.client.AuthRequest;
-import com.google.api.gwt.oauth2.client.Callback;
 import com.google.common.collect.Maps;
+import com.google.gwt.core.client.Callback;
 import com.google.gwt.event.shared.EventBus;
 
 import java.util.Map;
